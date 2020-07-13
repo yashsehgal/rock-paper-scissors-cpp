f83cb710125858d14df277a01472ae45fa99fe2f
yashsehgal <yashsehgal.work@gmail.com>
2020-07-14 01:38:51 +0530

    added some more conditional operational features in the game logic code

--- game-logic.h
+++ game-logic.h
@@ -30,7 +30,12 @@
 
 class GameLogic {
   private:
+  char computer_turn_options[10][3];
   public:
+  GameLogic() {
+    computer_turn_options[10][3] = {"rock", "paper", "scissors"};
+  }
+  // this function will return the user and computer score at every iteration of the gameplay.
   std::string getWinnerName(int user_score, int computer_score) {
     if (user_score > computer_score) {
       std::string statement = "user"; 
@@ -51,7 +56,7 @@ class GameLogic {
    * game logical events. The function will just return the score increament and decreament
    * values after comparing the user's moves.
   */
-  bool checkStatusEvent(char * user_turn, char * computer_turn) {
+  int checkStatusEvent(char * user_turn, char * computer_turn) {
     // checking if the input options are non-empty
     if (user_turn != '\0' && computer_turn != '\0') {
       // comparing the user-selection events
@@ -62,6 +67,7 @@ class GameLogic {
                user_turn == "P" || user_turn == "Paper") user_turn = "paper";
       else if (user_turn == "scissor" || user_turn == "s" || 
                user_turn == "S" || user_turn == "Scissor") user_turn = "scissor";
+      
       // converting all possible inputs to a specific keyword - for computer-turn
       if (computer_turn == "rock" || computer_turn == "Rock" || 
           computer_turn == "r" || computer_turn == "R") computer_turn = "rock";
@@ -69,6 +75,18 @@ class GameLogic {
                computer_turn == "P" || computer_turn == "Paper") computer_turn = "paper";
       else if (computer_turn == "scissor" || computer_turn == "s" || 
                computer_turn == "S" || computer_turn == "Scissor") computer_turn = "scissor";
+
+      // comparing game logical events
+      if (user_turn == "rock" && computer_turn == "rock") return -1;
+      else if (user_turn == "rock" && computer_turn == "paper") return 0; 
+      else if (user_turn == "rock" && computer_turn == "scissor") return 1;
+      else if (user_turn == "paper" && computer_turn == "rock") return 1;
+      else if (user_turn == "paper" && computer_turn == "paper") return -1;
+      else if (user_turn == "paper" && computer_turn == "scissor") return 0;
+      else if (user_turn == "scissor" && computer_turn == "rock") return 0;
+      else if (user_turn == "scissor" && computer_turn == "paper") return 1;
+      else if (user_turn == "scissor" && computer_turn == "scissor") return -1;
+      else return ;
     }
   }
   protected:
